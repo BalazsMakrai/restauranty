99796908f57e2a6e003296fb930d17f825f11ea7
Balazs Makrai <b.makrai@gmail.com>
2022-12-01 11:51:45 +0100

    add table

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Restaurantsly</a>
+            <a class="navbar-brand" href="/">Restaurantsly</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
                     <li class="nav-item">
